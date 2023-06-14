6a2812ee75f8c15deaec7a31891b003628c629ed
dmattern14 <130599602+dmattern14@users.noreply.github.com>
2023-06-14 11:45:40 -0600

    link debug
    li

--- missingapp/models.py
+++ missingapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+class Person (models.Model):
+    pass
+    
 # Create your models here.
 
-#class person (models.Model):
 # use class meta: db_table = "container" to link to container table on missingpersons.html
