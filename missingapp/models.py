9f4b5a4f59c192a03a64d0606497c34335435a11
dmattern14 <130599602+dmattern14@users.noreply.github.com>
2023-06-14 14:52:00 -0600

    fixed third page links

--- missingapp/models.py
+++ missingapp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 class Person (models.Model):
-    pass
+    dateMissing = models.DateField()
+lastName = models.CharField(max_length=50)
+firstName = models.CharField(max_length=50)
+ageMissing = models.IntegerField()
     
 # Create your models here.
 
