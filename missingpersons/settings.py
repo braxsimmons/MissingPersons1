6c69a23e65d85b9b4ff3900a103e74e9e96a770d
bamcavoy <120762820+bamcavoy@users.noreply.github.com>
2023-06-20 13:29:40 -0600

    highlight home on home

--- missingpersons/settings.py
+++ missingpersons/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'missing_persons',
     'USER': 'postgres',
-    'PASSWORD': '',
+    'PASSWORD': 'IS3032023',
     'HOST': '127.0.0.1',
     'PORT': '5432',
     }
