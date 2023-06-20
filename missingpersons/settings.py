dde0595a43deb56ff058da309010fc3b09d0987a
mckayabbott <120704571+mckayabbott@users.noreply.github.com>
2023-06-20 12:21:59 -0600

    Change password to blank

--- missingpersons/settings.py
+++ missingpersons/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'missing_persons',
     'USER': 'postgres',
-    'PASSWORD': '787346',
+    'PASSWORD': '',
     'HOST': '127.0.0.1',
     'PORT': '5432',
     }
